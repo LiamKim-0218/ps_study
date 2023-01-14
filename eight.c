93a174c9b7dbbf962d606b0ba390e78c53aba71f
LiamKim-0218 <a01071325344@gmail.com>
2023-01-14 10:00:40 +0900

    배열의 평균값

--- eight.c
+++ eight.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 
 // numbers_len은 배열 numbers의 길이입니다.
 double solution(int numbers[], size_t numbers_len) {
     double answer = 0;
-    answer=   (numbers[0] + strlen(numbers_len-1)/2);
-    printf("%d");
+    for(int i=0;i<numbers_len;i++)
+    {
+        answer += numbers[i];
+    }
+    answer /= numbers_len;
     return answer;
 }
