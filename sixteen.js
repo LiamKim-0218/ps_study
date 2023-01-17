34969f926ecd8330b85819ed4a563dd10cf8e211
LiamKim-0218 <a01071325344@gmail.com>
2023-01-18 08:37:13 +0900

    문자열 뒤집기

---
+++ sixteen.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    var answer = '';
+    for(let i = my_string.length-1; i>=0; i--)
+        {
+            answer+=my_string[i];
+        }
+    return answer;
+}
+
+
+function solution(my_string) {
+    return my_string.split('').reverse().join('');
+}
