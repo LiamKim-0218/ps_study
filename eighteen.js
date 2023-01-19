78d3b858dc0a1f085178db6b923455a8045081d4
LiamKim-0218 <a01071325344@gmail.com>
2023-01-19 19:49:36 +0900

    eighteen.js

---
+++ eighteen.js
@@ -0,0 +1,16 @@
+function solution(strlist) {
+    var answer = [];
+    for(let i=0;i<strlist.length;i++)
+        {
+            answer[i]=strlist[i].length;
+            // for(let j=0; j<strlist[i].length;j++)
+            //     {
+            //         if(strlist[i][j]===true)
+            //             {
+            //                 i++;
+            //                 break;
+            //             }
+            //     }
+        }
+    return answer;
+}
