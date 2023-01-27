d2a898d53921b9f6efcaa31135fdfd833a712d4f
LiamKim-0218 <a01071325344@gmail.com>
2023-01-27 09:00:46 +0900

    n numbers spaced by x.py

--- week3/n numbers spaced by x.py
+++ week3/n numbers spaced by x.py
@@ -1,22 +1,28 @@
-# def solution(x, n):
-#     answer = []
-#     #if x>0:
-#     for i in list(range(x,x*(n+1),x)):
-#         answer.append(i)
-#         print(i)    
-#     # if x<0:
-#     #     for i in list(range(x,x*n-1,x)):
-#     #         answer.append(i)
-#     #         print(i)
-#     # if x == 0: 
-#     #     answer.append(x)
-#     if x == 0: 
-#             answer=0
-#     return answer
 def solution(x, n):
     answer = []
-    buf = x
-    for x in range(1,n+1):
-        print(x)
-        answer.append(buf*x)
+    if x!=0:
+        for i in range(x,x*(n+1),x):
+            answer.append(i)
+            print(i)    
+    # if x<0:
+    #     for i in list(range(x,x*n-1,x)):
+    #         answer.append(i)
+            # print(i)
+    # if x == 0: 
+    #     answer.append(x)
+    if x == 0: 
+        answer = [0] * n
+        # for i in range(n):
+        #     answer.append(0)
     return answer
+
+
+
+
+# def solution(x, n):
+#     answer = []
+#     buf = x
+#     for x in range(1,n+1):
+#         # print(x)
+#         answer.append(buf*x)
+#     return answer
